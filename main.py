2dc4953f633f02a8fc1dc4c8bdd4382dcce59f8b
Vivek Calambur <calambu2@illinois.edu>
2018-03-21 23:38:46 -0700

    connection working on local and app engine

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import os
+import MySQLdb
+from flask import Flask
+
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_DB = os.environ.get('CLOUDSQL_DB')
+
+def connect_to_cloudsql():
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        cloudsql_unix_socket = os.path.join('/cloudsql', 
+            CLOUDSQL_CONNECTION_NAME)
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            db=CLOUDSQL_DB)
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1',
+            port=3307, 
+            user=CLOUDSQL_USER,
+            db=CLOUDSQL_DB)
+
+    return db
 
 app = Flask(__name__)
-db = SQLAlchemy()
-app.config.from_pyfile('config.py')
-db.init_app(app)
+@app.route("/")
+def hello():
+    db = connect_to_cloudsql()
+    cursor = db.cursor()
+    cursor.execute("SHOW TABLES")
+    temp = cursor.fetchone()
+    return temp
 
-with app.app_context():
-    result = db.engine.execute('show tables;')
-    rows = result.fetchall()
-    print rows[0][0]
-
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
-    return render_template(
-        'submitted_form.html',
-        name=rows,
-        email=email,
-        site=site,
-        comments=comments)
