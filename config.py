b3dcfe708947a71aa92300d7fc539c6a9ee793b9
Vivek Calambur <calambu2@illinois.edu>
2018-03-21 18:55:59 -0700

    made changes

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
-PROJECT_ID = 'your-project-id'
+PROJECT_ID = 'scurt-198704'
 
 CLOUDSQL_USER = 'root'
-CLOUDSQL_PASSWORD = 'your-cloudsql-password'
-CLOUDSQL_DATABASE = 'bookshelf'
+CLOUDSQL_DATABASE = 'sCUrt_db'
 # Set this value to the Cloud SQL connection name, e.g.
 #   "project:region:cloudsql-instance".
 # You must also update the value in app.yaml.
-CLOUDSQL_CONNECTION_NAME = 'your-cloudsql-connection-name'
+CLOUDSQL_CONNECTION_NAME = 'scurt-198704:us-central1:mysql-1'
 
 SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{user}:{password}@localhost/{database}'
+    'mysql+pymysql://{user}@localhost/{database}'
     '?unix_socket=/cloudsql/{connection_name}').format(
-        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
-        database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
+        user=CLOUDSQL_USER, database=CLOUDSQL_DATABASE, 
+        connection_name=CLOUDSQL_CONNECTION_NAME)
