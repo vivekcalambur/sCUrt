988d31b2cc7b28b426dc9bfce21d49f9a3dfa2be
Vivek Calambur <calambu2@illinois.edu>
2018-03-21 18:47:39 -0700

    finished config

---
+++ config.py
@@ -0,0 +1,15 @@
+PROJECT_ID = 'your-project-id'
+
+CLOUDSQL_USER = 'root'
+CLOUDSQL_PASSWORD = 'your-cloudsql-password'
+CLOUDSQL_DATABASE = 'bookshelf'
+# Set this value to the Cloud SQL connection name, e.g.
+#   "project:region:cloudsql-instance".
+# You must also update the value in app.yaml.
+CLOUDSQL_CONNECTION_NAME = 'your-cloudsql-connection-name'
+
+SQLALCHEMY_DATABASE_URI = (
+    'mysql+pymysql://{user}:{password}@localhost/{database}'
+    '?unix_socket=/cloudsql/{connection_name}').format(
+        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+        database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
